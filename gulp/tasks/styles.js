d151bc3b11507f7cd20c3ffc79b856ade1f0f1fd
Rens <rens@studiodrieluik.nl>
2017-02-21 12:14:24 +0100

    Project so far

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
+var mixins = require('postcss-mixins');
+
+gulp.task('styles', function(){
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString()); // error message toont in terminal
+      this.emit('end'); // browsersync blijft actief ondanks error
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
+
+});
